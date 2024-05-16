4a6897e59b3fc966e07992b8d6cf43e8d9a9a8cc
HowardChen123 <howard.chenhc@gmail.com>
2024-05-16 19:21:17 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,34 +1,72 @@
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * Solve the FizzBuzz challenge.
  */
 class FizzBuzz {
 
+    static String fizzBuzzWhileLoop(int n) {
+
+        int i = 1;
+        List<String> res = new ArrayList<String>();
+
+        while (i < n) {
+
+            doFizzBuzz(i, res);
+            i ++;
+
+        }
+        return res.toString();
+
+    }
+
+    private static void doFizzBuzz(int i, List<String> res) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            res.add("FizzBuzz");
+
+        } else if (divisibleBy3) {
+
+            res.add("Fizz");
+
+        } else if (divisibleBy5) {
+
+            res.add("Buzz");
+
+        } else {
+
+            res.add(Integer.toString(i));
+
+        }
+    }
+
+    static String fizzBuzzForLoop(int n) {
+
+        List<String> res = new ArrayList<String>();
+
+        for (int i = 1; i < n; i++) {
+
+            doFizzBuzz(i, res);
+
+        }
+        return res.toString();
+    }
+
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        String forResult = fizzBuzzForLoop(10);
+        String whileRes = fizzBuzzWhileLoop(10);
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+        System.out.println(forResult);
+        System.out.println(whileRes);
 
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+        System.out.println(whileRes.equals(forResult));
 
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
-        }
     }
 }
